a5498d53b9bf6dca4b5f835c2f623dd8106ac94f
mossygrave <mmckay03@byui.edu>
2024-09-21 17:33:56 -0600

    Added notes to sanbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
+// For refrence this is how comments work 
+
 class Program
 {
     static void Main(string[] args)
     {
+        //print example
         Console.WriteLine("Hello Sandbox World!");
+        //WriteLine takes up the whole line while Write does not
+
+        //input and var creation example
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+ 
+        //using a variable in a string example
+        Console.WriteLine($"Your favorite color is {color}?");
+
+        // if statement example
+        if (color == "purple")
+        {
+            Console.WriteLine("Thats my favorite color too!");
+            Console.WriteLine("");
+        }
+        else if (color == "yellow") //note c# uses else if rather than elif
+        {
+            Console.WriteLine("That's a good color");
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine("Thats a cool color I guess");
+            Console.WriteLine("");
+        }
+
+        //Operands and -> && or -> || not -> !
+
+        //once defined a variable can't change type but you can create a new variable with the desired type
+        Console.WriteLine("What is your favorite number? ");
+        //always returns a string
+        string faveNum = Console.ReadLine();
+        //moves the number into an int variable
+        int num = int.Parse(faveNum);
+        //.ToString() converts to a string
     }
 }
