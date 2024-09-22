38f638e28e76b2ac452f5642aaf4ef67fd1c8576
mossygrave <mmckay03@byui.edu>
2024-09-21 18:31:04 -0600

    completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int num = -1;
+        int numberOfNumbers = 0;
+        int largestNumber = 0;
+
+        //Collecting all numbers from user
+        while (num != 0)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            num = int.Parse(input);
+            numbers.Add(num);
+            numberOfNumbers++;
+            if (num > largestNumber)
+            {
+                largestNumber = num;
+            }
+        }
+
+        //Adding all the numbers in the list
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        //finding the average 
+        float average = ((float)sum) / (numberOfNumbers - 1);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
