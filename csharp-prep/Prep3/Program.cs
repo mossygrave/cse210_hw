444142694e9b3e78753ca80b4ad030043acb2031
mossygrave <mmckay03@byui.edu>
2024-09-21 18:04:17 -0600

    completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess the magic number ");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        int guess;
+        string input;
+        int guessesNumber = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+            Console.WriteLine("");
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.WriteLine("");
+            guessesNumber++;
+
+        } while (guess != magic);
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"It took you {guessesNumber} tries.");
+        Console.WriteLine("");
     }
 }
