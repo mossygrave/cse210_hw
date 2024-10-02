7e2c30e634b4824e3f2ed7b7f79380606763b003
mossygrave <mmckay03@byui.edu>
2024-10-02 11:35:09 -0600

    Fraction program completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+
+        // fraction1.DisplayFraction();
+        // fraction2.DisplayFraction();
+        // fraction3.DisplayFraction();
+
+        string fractionString = fraction3.GetFractionString();
+        Console.WriteLine(fractionString);
+
+        double decimalValue = fraction3.GetDecimalValue();
+        Console.WriteLine(decimalValue);
     }
 }
